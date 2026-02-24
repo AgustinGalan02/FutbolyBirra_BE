b83fb06f6e2949a3ef082575a6ea882029678599
AgustinGalan02 <agustingalan4@gmail.com>
2026-02-24 00:12:43 -0300

    modified categoryController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,7 +7,7 @@ export const createCategory = async (req, res) => {
     try {
         const newCategory = new Category({ title, description });
         const savedCategory = await newCategory.save();
-        res.status(201).json(savedCategory); 
+        res.status(201).json(savedCategory);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -17,7 +17,23 @@ export const createCategory = async (req, res) => {
 export const getCategories = async (req, res) => {
     try {
         const categories = await Category.find();
-        res.json(categories);
+        const categoriesWithLastPost = await Promise.all(
+            categories.map(async (cat) => {
+                // Buscamos el post más reciente que coincida con esta categoria
+                const lastPost = await Post.findOne({ category: cat._id })
+                    .sort({ createdAt: -1 }) // Ordenar por fecha (descendente)
+                    .populate('author', 'username'); // Traer el nombre del autor
+
+                return {
+                    ...cat._doc,
+                    lastPostTitle: lastPost ? lastPost.title : null,
+                    lastPostAuthor: lastPost ? lastPost.author.username : null,
+                    lastPostDate: lastPost ? lastPost.createdAt : null,
+                };
+            })
+        );
+
+        res.json(categoriesWithLastPost);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -36,7 +52,7 @@ export const getCategoriesById = async (req, res) => {
 
 //PUT
 export const updateCategory = async (req, res) => {
-    const { id } = req.params; 
+    const { id } = req.params;
     try { // Actualizamos la categoria
         const categoryUpdated = await Category.findByIdAndUpdate(id, req.body, { new: true });
         if (!categoryUpdated) return res.status(404).json({ message: "Category not found" });
